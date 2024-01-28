a050934aa046a3fdf9ad01086ee67eaffa8f1269
DemerNkardaz <92405785+DemerNkardaz@users.noreply.github.com>
2024-01-28 02:07:30 +0200

    F

--- default.js
+++ default.js
@@ -1,16 +1,64 @@
+import * as $item from './variables.js';
+
 function siteTitleOnLang() {
 
     var userLanguage = navigator.language || navigator.userLanguage;
 
     if (userLanguage.toLowerCase() === 'ru') {
-        $('#siteTitle').text('Нкардазолинк');
+        $item.siteTitle.text('Нкардазолинк');
     } else {
-        $('#siteTitle').text('Nkardazolink');
+        $item.siteTitle.text('Nkardazolink');
     }
+}
+function headerInit() {
+    $(':header').each(function () {
+        const headerLevel = parseInt(this.tagName.substring(1));
+        const baseSize = 1.75;
+        const newSize = baseSize - (headerLevel - 1) * 0.15;
+        $(this).css({ 'font-size': 'calc(' + newSize + 'em)' });
+        $(this).addClass('headerDefault');
+    });
 }
 
 
-
 $(document).ready(function () {
+    headerInit();
     siteTitleOnLang();
+
+    const color_gold_hover = $(':root').css('--color_gold_hover');
+
+
+    $item.personFlexButton.on({
+        mouseover: function () {
+            $(this).find('.material-icons').css('color', color_gold_hover).css('transform', 'translate(-12px, -12px) rotate(-180deg)').css('text-shadow', '0 -1px 2px var(--shadow_half)');
+        },
+        mouseout: function () {
+            $(this).find('.material-icons').removeAttr('style');
+        }
+    })
+
+    $item.settingsOpt.parent().on('click', function () {
+        $item.blockumInformatorum.toggleClass('opens-settings');
+        if (!$item.blockumInformatorum.hasClass('active')) {
+            $item.blockumInformatorum.addClass('active');
+        } else if (!$item.blockumInformatorum.hasClass('opens-moreInfo')) {
+            $item.blockumInformatorum.removeClass('active');
+        }
+        $item.blockumInformatorum.removeClass('opens-moreInfo');
+
+    });
+
+
+    $item.moreInfoOpt.parent().on('click', function () {
+        $item.blockumInformatorum.toggleClass('opens-moreInfo');
+        if (!$item.blockumInformatorum.hasClass('active')) {
+            $item.blockumInformatorum.addClass('active');
+        } else if (!$item.blockumInformatorum.hasClass('opens-settings')) {
+            $item.blockumInformatorum.removeClass('active');
+        }
+        $item.blockumInformatorum.removeClass('opens-settings');
+
+    });
+
+
 })
---
+++ variables.js
@@ -0,0 +1,55 @@
+let personFlex;
+let personFlexButton;
+let personPlate;
+let personName;
+let personBlock;
+let personInfo;
+let personBanner;
+let personBannerImg;
+let personAvatar;
+let personAvatarImg;
+let siteTitle;
+let settingsOpt;
+let moreInfoOpt;
+let blockumInformatorum;
+let moreInfoBlock;
+let settingsBlock;
+
+$(document).ready(function () {
+    personFlex = $('.personFlex');
+    personFlexButton = $('.personFlexButton');
+    personPlate = $('.personPlate');
+    personName = $('#personName');
+    personBlock = $('#personBlock');
+    personInfo = $('.personInfo');
+    personBanner = $('#personBanner');
+    personBannerImg = $('#personBannerImg');
+    personAvatar = $('#personAvatar');
+    personAvatarImg = $('#personAvatarImg');
+    siteTitle = $('#siteTitle');
+    settingsOpt = $('#settingsOpt');
+    moreInfoOpt = $('#moreInfoOpt');
+    blockumInformatorum = $('.blockumInformatorum');
+    moreInfoBlock = $('#moreInfoBlock');
+    settingsBlock = $('#settingsBlock');
+
+});
+
+export {
+    personFlex,
+    personFlexButton,
+    personPlate,
+    personName,
+    personBlock,
+    personInfo,
+    personBanner,
+    personBannerImg,
+    personAvatar,
+    personAvatarImg,
+    siteTitle,
+    settingsOpt,
+    moreInfoOpt,
+    blockumInformatorum,
+    moreInfoBlock,
+    settingsBlock
+};
