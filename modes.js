1b8afe31966e535956af58fc4448b2049b58799d
DemerNkardaz <92405785+DemerNkardaz@users.noreply.github.com>
2024-02-24 16:15:28 +0200

    A

--- modes.js
+++ modes.js
@@ -5,9 +5,12 @@ if (mode === 'cv') {
   $('#miniFolioBlock').remove();
   $('#personLinksBlock').remove();
   $('.personFlexButton').remove();
+  $('#personInformationBlock').children('.personDescription').eq(0).children().remove();
   $('#personName').parent().addClass('cv_mode');
 
+
+  
   $('#personInformationBlock').children('.personDescription').eq(0).load('cv.html #cv_main_description');
   $('#personInformationBlock').children('.personDescription').eq(0).clone().insertAfter('#personInformationBlock .personDescription:eq(0)').load('cv.html #cv_main_socials');
-
+  $('#personInformationBlock').children('.personDescription').eq(0).clone().insertAfter('#personInformationBlock .personDescription:eq(1)').load('cv.html #cv_second_socials').addClass('cv_mode');
 }
