62ee6d536429b7c6b4734b76627cc7525edecee2
DemerNkardaz <92405785+DemerNkardaz@users.noreply.github.com>
2024-02-25 10:02:06 +0200

    А

--- modes.js
+++ modes.js
@@ -36,19 +36,27 @@ if (mode === 'cv') {
   $('.personFlexButton').remove();
   $('.personKamon').remove();
   $('#personInformationBlock').children('.personDescription').eq(0).children().remove();
+  $('#personBlock').children('.blockumInformatorum').eq(0).remove();
   $('#personInformationBlock').addClass('cv_mode');
   $('#personName').parent().addClass('cv_mode');
   $('[data-key="SaitamaOnmyoji"]').after($('<span>').text('Bălți, Republica Moldova'));
   $('[data-key="SaitamaOnmyoji"').remove();
 
   var cv_description;
+  var cv_contents;
   if (sel === '3d') {
     cv_description = '#cv_main_description_3d';
+    cv_contents = '#cv_contents_3d';
   } else if (sel === '2d') {
     cv_description = '#cv_main_description_2d';
+    cv_contents = '#cv_contents_2d';
   }
   
   $('#personInformationBlock').children('.personDescription').eq(0).load('cv.html ' + cv_description);
   $('#personInformationBlock').children('.personDescription').eq(0).clone().insertAfter('#personInformationBlock .personDescription:eq(0)').load('cv.html #cv_main_socials');
   $('#personInformationBlock').children('.personDescription').eq(0).clone().insertAfter('#personInformationBlock .personDescription:eq(1)').load('cv.html #cv_second_socials').addClass('cv_mode');
+
+
+  $('#linkBlock').prepend('<div id="CVList" class="cv_page"><div id="CVListWrapper"></div></div>');
+  $('#CVListWrapper').eq(0).load('cv.html ' + cv_contents);
 }
