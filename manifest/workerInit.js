d1ed7772b6b16dfd4caf1de8714277088e323e9c
DemerNkardaz <92405785+DemerNkardaz@users.noreply.github.com>
2024-05-22 06:15:39 +0300

    А

--- manifest/workerInit.js
+++ manifest/workerInit.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-  .register('../serviceworker.js')
+  .register('./serviceworker.js')
   .then(reg => console.log('Registration succeeded. Scope is ' + reg.scope))
   .catch(error => console.log('Registration failed with ' + error));
 }
